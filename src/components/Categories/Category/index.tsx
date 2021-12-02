39836db1fb3cdbb02bfb4c111d3669092b252b83
GeraldineBB <geraldine-bornet@hotmail.fr>
2021-12-02 18:07:11 +0100

    feat : component categories

---
+++ src/components/Categories/Category/index.tsx
@@ -0,0 +1,40 @@
+// import './styles.scss';
+import Card from "@mui/material/Card";
+import Typography from "@mui/material/Typography";
+import {CardMedia} from "@mui/material";
+import CardContent from "@mui/material/CardContent";
+import { CardActionArea } from '@mui/material';
+
+
+import music from "../../../assets/illustrations/music.svg";
+
+
+const Category = () => {
+
+    return (
+        <Card className="card">
+        <CardActionArea>
+
+            <CardContent>
+            <CardMedia
+                component="img"
+                sizes="50"
+                image={music}
+                alt="music-illustrations"
+            />
+            <Typography variant="h5" component="div">
+                Musique
+            </Typography>
+            
+            </CardContent>
+        </CardActionArea>
+
+      </Card>
+    ); 
+
+
+}
+
+
+
+export default Category;
