955961d9eb0ad448d1a40e2f162e9141d5227ec0
GeraldineBB <geraldine-bornet@hotmail.fr>
2021-12-02 12:00:39 +0100

    wip: presentation bloc without responsive

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../Header';
+import Slogan from '../Slogan'; 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Slogan />
     </div>
   );
 }
