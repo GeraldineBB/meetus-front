f6ac2c28d36468ec8cd6031780d8e6449d18120d
Muhammad Zelliche <tetoshe@macbook-pro-de-tetoshe.home>
2021-12-02 09:52:11 +0100

    config mui dependencies added

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,8 +4,8 @@ import Button from '@mui/material/Button';
 function Header() {
     return (
         <div className='Header'>
-            <Button className='header__button__login' variant="contained">Se connecter</Button>;
-            <Button className='header__button__signUp' variant="contained">S'inscrire</Button>;
+            <Button className='header__button__login' variant="contained">Se connecter</Button>
+            <Button className='header__button__signUp' variant="contained">S'inscrire</Button>
         </div>
     )
 }
