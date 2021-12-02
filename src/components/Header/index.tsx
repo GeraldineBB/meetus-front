e52ccddb3c095e6c7c62494c33527c2d0b989f30
Muhammad Zelliche <tetoshe@macbook-pro-de-tetoshe.home>
2021-12-02 09:45:27 +0100

    whip removal of a basic react component and installation of the header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import './style.scss';
+import Button from '@mui/material/Button';
+
+function Header() {
+    return (
+        <div className='Header'>
+            <Button className='header__button__login' variant="contained">Se connecter</Button>;
+            <Button className='header__button__signUp' variant="contained">S'inscrire</Button>;
+        </div>
+    )
+}
+
+export default Header;
