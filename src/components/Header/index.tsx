f82c5f002166c8cab66036135fad1a08c4035921
Muhammad Zelliche <tetoshe@macbook-pro-de-tetoshe.home>
2021-12-02 10:13:58 +0100

    whip fix position of login button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,15 @@
 import './style.scss';
 import Button from '@mui/material/Button';
-
+import Logo from '../../assets/logo/logo.svg';
+ 
 function Header() {
     return (
-        <div className='Header'>
-            <Button className='header__button__login' variant="contained">Se connecter</Button>
-            <Button className='header__button__signUp' variant="contained">S'inscrire</Button>
+        <div className='header'>
+            <img src={Logo} alt="Logo" />
+            <Button className='header__button__login' color='success' variant="contained">Se connecter</Button>
+            <Button className='header__button__signup' variant="contained">S'inscrire</Button>
         </div>
     )
-}
+};
 
 export default Header;
