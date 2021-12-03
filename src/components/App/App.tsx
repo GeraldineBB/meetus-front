3df5f33aed01faaf35b6608fae7ebd81fa96a499
Muhammad Zelliche <tetoshe@macbook-pro-de-tetoshe.home>
2021-12-03 11:31:30 +0100

    fix bug header

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,14 +4,13 @@ import {
 } from "react-router-dom";
 
 import HomePage from '../../Views/HomePage';
-import Login from "../../Views/Login";
+
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path='/' element={<HomePage />} />
-        <Route path='/login' element={<Login />} />
       </Routes>
     </div>
   
