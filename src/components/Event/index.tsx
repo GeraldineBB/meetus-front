e159fec22ef096dee5ba2c04580cca9d37387242
JeremyDavito <jeremy.davito@gmail.com>
2021-12-03 15:48:36 +0100

    fix css event form, todo fix the two buttoms

--- src/components/Event/index.tsx
+++ src/components/Event/index.tsx
@@ -2,17 +2,16 @@ import React from 'react';
 import EventForm from './EventForm';
 import Button from '@mui/material/Button';
 import Header from '../../components/Header';
+import './style.scss';
 const Event = () => {
 
     return (
 
         <div>
             <Header />
+            <div className='event'>
             <EventForm />
-            
-            {/* LE BUTTON ICI EST PR LEXEMPLE, IL IRA EN SUBMIT DANS LE EVENT FORM, A CAUSE DE TYPESCRIPT NE PEUT PAS ENCORE LE TESTER */}
-            <Button sx={{ backgroundColor: '#F36B7F', '&:hover': { backgroundColor: '#F8CF61' } }}  className='buttom' variant="contained">Créer mon évènement</Button>
-
+            </div>
         </div>
 
     );
