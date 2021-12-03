b9311684e2fae38c4c35956f9ed5d0541d02e3d5
GeraldineBB <geraldine-bornet@hotmail.fr>
2021-12-03 11:38:55 +0100

    wip : reducer, middleware, store to test datas format

--- src/data/events.js
+++ src/data/events.js
@@ -1,79 +1,29 @@
-[
+export default events = [
     {
       "id": 1,
       "title": "Titre :0",
       "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam egestas bibendum ipsum, a blandit tellus aliquet eu. Sed ac odio non odio posuere auctor. Donec lobortis egestas aliquam. Duis venenatis.",
       "picture": "event_placeholder.png",
-      "date": "2021-12-03T10:16:35+01:00"
+      "date": "2021-12-03T10:16:35+01:00",
+      "city" : "Marseille",
+      "members" : 10, 
     },
     {
       "id": 2,
       "title": "Titre :1",
       "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam egestas bibendum ipsum, a blandit tellus aliquet eu. Sed ac odio non odio posuere auctor. Donec lobortis egestas aliquam. Duis venenatis.",
       "picture": "event_placeholder.png",
-      "date": "2021-12-03T10:16:35+01:00"
+      "date": "2021-12-03T10:16:35+01:00",
+      "city" : "Marseille",
+      "members" : 10, 
     },
     {
       "id": 3,
       "title": "Titre :2",
       "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam egestas bibendum ipsum, a blandit tellus aliquet eu. Sed ac odio non odio posuere auctor. Donec lobortis egestas aliquam. Duis venenatis.",
       "picture": "event_placeholder.png",
-      "date": "2021-12-03T10:16:35+01:00"
-    },
-    {
-      "id": 4,
-      "title": "Titre :3",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam egestas bibendum ipsum, a blandit tellus aliquet eu. Sed ac odio non odio posuere auctor. Donec lobortis egestas aliquam. Duis venenatis.",
-      "picture": "event_placeholder.png",
-      "date": "2021-12-03T10:16:35+01:00"
-    },
-    {
-      "id": 5,
-      "title": "Titre :4",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam egestas bibendum ipsum, a blandit tellus aliquet eu. Sed ac odio non odio posuere auctor. Donec lobortis egestas aliquam. Duis venenatis.",
-      "picture": "event_placeholder.png",
-      "date": "2021-12-03T10:16:35+01:00"
-    },
-    {
-      "id": 6,
-      "title": "Titre :5",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam egestas bibendum ipsum, a blandit tellus aliquet eu. Sed ac odio non odio posuere auctor. Donec lobortis egestas aliquam. Duis venenatis.",
-      "picture": "event_placeholder.png",
-      "date": "2021-12-03T10:16:35+01:00"
-    },
-    {
-      "id": 7,
-      "title": "Titre :6",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam egestas bibendum ipsum, a blandit tellus aliquet eu. Sed ac odio non odio posuere auctor. Donec lobortis egestas aliquam. Duis venenatis.",
-      "picture": "event_placeholder.png",
-      "date": "2021-12-03T10:16:35+01:00"
-    },
-    {
-      "id": 8,
-      "title": "Titre :7",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam egestas bibendum ipsum, a blandit tellus aliquet eu. Sed ac odio non odio posuere auctor. Donec lobortis egestas aliquam. Duis venenatis.",
-      "picture": "event_placeholder.png",
-      "date": "2021-12-03T10:16:35+01:00"
-    },
-    {
-      "id": 9,
-      "title": "Titre :8",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam egestas bibendum ipsum, a blandit tellus aliquet eu. Sed ac odio non odio posuere auctor. Donec lobortis egestas aliquam. Duis venenatis.",
-      "picture": "event_placeholder.png",
-      "date": "2021-12-03T10:16:35+01:00"
-    },
-    {
-      "id": 10,
-      "title": "Titre :9",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam egestas bibendum ipsum, a blandit tellus aliquet eu. Sed ac odio non odio posuere auctor. Donec lobortis egestas aliquam. Duis venenatis.",
-      "picture": "event_placeholder.png",
-      "date": "2021-12-03T10:16:35+01:00"
-    },
-    {
-      "id": 11,
-      "title": "Titre :10",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam egestas bibendum ipsum, a blandit tellus aliquet eu. Sed ac odio non odio posuere auctor. Donec lobortis egestas aliquam. Duis venenatis.",
-      "picture": "event_placeholder.png",
-      "date": "2021-12-03T10:16:35+01:00"
+      "date": "2021-12-03T10:16:35+01:00",
+      "city" : "Marseille",
+      "members" : 10, 
     }
-  ] 
+] 
