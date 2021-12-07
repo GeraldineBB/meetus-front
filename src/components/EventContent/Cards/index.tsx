e8ebf868b19be56e8e01654b9fe2f8029ae0f1bc
GeraldineBB <geraldine-bornet@hotmail.fr>
2021-12-07 15:07:53 +0100

    style component eventContent

---
+++ src/components/EventContent/Cards/index.tsx
@@ -0,0 +1,80 @@
+import * as React from "react";
+import { Link } from 'react-router-dom';
+
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { CardMedia } from "@mui/material";
+// import Image from "../../../../assets/images/fire-event.jpg";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
+
+interface EventCardProps {
+  picture: string;
+  date: string;
+  city: string;
+  membersCount: number;
+  title: string;
+  description: string;
+  id: number; 
+}
+
+export default function EventCard({
+  picture,
+  date,
+  city,
+  membersCount,
+  title,
+  description,
+  id, 
+}: EventCardProps) {
+  return (
+    <Card className="card" sx={{ mr: 3 }}>
+      <CardContent>
+        <CardMedia
+          component="img"
+          sizes="100"
+          image={`${process.env.PUBLIC_URL}/images/${picture}`}
+          alt="Paella dish"
+        />
+        <Typography variant="h5" component="div">
+          {title}
+        </Typography>
+        <Typography color="text.primary">
+          <LocationOnIcon fontSize="medium" />
+          {city}
+        </Typography>
+        <Typography sx={{ mb: 1.5 }}>
+          <CalendarTodayIcon />
+          {date}
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} variant="body1">
+          {description}
+        </Typography>
+        <Typography color="text.primary">
+          {membersCount} participants
+        </Typography>
+      </CardContent>
+      <CardActions>
+      <Link to={`/events/${id}`}>
+      
+        <Button
+        size="small"
+        variant="contained"
+        className="card__button"
+        sx={{
+          width: "100%",
+          backgroundColor: "#F36B7F",
+          "&:hover": { backgroundColor: "#F8CF61" },
+        }}
+      >
+        En savoir plus
+      </Button>
+      </Link>
+      
+      </CardActions>
+    </Card>
+  );
+}
--- src/components/EventContent/index.js
+++ src/components/EventContent/index.js
@@ -9,6 +9,12 @@ import DayJS from "react-dayjs";
 // import 'dayjs/locale/te'; 
 import "dayjs/locale/fr";
 
+import Cards from "./Cards";
+import LinkSection from "../../components/HomePage/LinkSection";
+
+import Grid from "@mui/material/Grid";
+import { Container } from "@mui/material";
+
 
 import { LOAD_INFO_FOR_PAGE_EVENT } from '../../actions/events';
 
@@ -35,79 +41,97 @@ const EventContent = () => {
     if (loading) {
         return <div>coucou</div>;
     }
-    const date = {...eventInfoPage.event.date}; 
 
       return (
 
-        <div className="eventContent">
-          <div className="eventContent__info">
-      
-              <div className="eventContent__info__header">
-                  <p className="eventContent__info__header--date">
-                    <DayJS
-                    format="dddd D MMMM YYYY" 
-                    locale="fr"
-                    {...eventInfoPage.event.date}
-                    >
-                    </DayJS> 
-                    </p>
-                  <p className="eventContent__info__header--title">
+ 
+        <div className="eventPage">
+          <div className="eventContent">
+            <div className="eventContent__info">
+                <div className="eventContent__info__header">
+                    <p className="eventContent__info__header--title">
                     {eventInfoPage.event.title}
-                  </p>
-                  <p className="eventContent__info__header--author">
-                    Crée par {eventInfoPage.event.author.fullName}
-                  </p>
-              </div>
-      
-              <div className="eventContent__info__picture">
-                  <img src={`${process.env.PUBLIC_URL}/images/concert-event.jpg`}/>
-              </div>
-      
-              <div className="eventContent__info__description">
-                  <p className="eventContent__info__description--text">
-                  {eventInfoPage.event.description}
-                  </p>
-              </div>
-      
-          </div>
-      
-      
-          <div className="eventContent__detail">
-              <div className="eventContent__detail__map">
-                  <img src={`${process.env.PUBLIC_URL}/illustrations/eventPage.svg`}/>
-              </div>
-              <p className="eventContent__detail__date">
-                {eventInfoPage.event.date}
-              </p>
-              <p className="eventContent__detail__hour">
-                {eventInfoPage.event.date}
-              </p>
-              <p className="eventContent__detail__adress">
-                {eventInfoPage.event.address}
-              </p>
-              <p className="eventContent__detail__zipcode">
-                {eventInfoPage.event.zipcode}
-              </p>
-      
-              <Button
-              size="small"
-              variant="contained"
-              className="card__button"
-              sx={{
-                width: "60%",
-                backgroundColor: "#F36B7F",
-                "&:hover": { backgroundColor: "#F8CF61" },
-              }}
-            >
-             Rejoindre
-            </Button>
-      
-              <p className="eventContent__detail__membersCount">
-              {eventInfoPage.event.membersCount} Participants
-              </p>
+                    </p>
+                    <p className="eventContent__info__header--date">
+                      <DayJS
+                      format="dddd D MMMM YYYY"
+                      locale="fr"
+                      {...eventInfoPage.event.date}
+                      >
+                      </DayJS>
+                      </p>
 
+                    <p className="eventContent__info__header--author">
+                      Crée par {eventInfoPage.event.author.fullName}
+                    </p>
+                </div>
+                <div className="eventContent__info__picture">
+                    <img src={`${process.env.PUBLIC_URL}/images/concert-event.jpg`}/>
+                </div>
+                <div className="eventContent__info__description">
+                    <p className="eventContent__info__description--text">
+                    {eventInfoPage.event.description}
+                    </p>
+                </div>
+            </div>
+            <div className="eventContent__detail">
+                <div className="eventContent__detail__map">
+                    <img src={`${process.env.PUBLIC_URL}/illustrations/eventPage.svg`}/>
+                </div>
+                <p className="eventContent__detail__date">
+                  <DayJS
+                  format="DD MM YYYY"
+                  locale="fr"
+                  {...eventInfoPage.event.date}
+                  >
+                  </DayJS>                
+                </p>
+                <p className="eventContent__detail__hour">
+                  <DayJS
+                  format="hh mm"
+                  locale="fr"
+                  {...eventInfoPage.event.date}
+                  >
+                  </DayJS>   
+                </p>
+                <p className="eventContent__detail__adress">
+                  {eventInfoPage.event.address}
+                </p>
+                <p className="eventContent__detail__zipcode">
+                  {eventInfoPage.event.zipcode} {eventInfoPage.event.city}
+                </p>
+                <Button
+                size="small"
+                variant="contained"
+                className="card__button"
+                sx={{
+                  width: "60%",
+                  backgroundColor: "#F36B7F",
+                  "&:hover": { backgroundColor: "#F8CF61" },
+                }}
+              >
+               Rejoindre
+              </Button>
+                <p className="eventContent__detail__membersCount">
+                {eventInfoPage.event.membersCount} Participants
+                </p>
+            </div>
           </div>
-
+          <div className="eventSimilar">
+          <LinkSection
+           title={"Evènements similaires"}
+           buttonName={"Voir tous les évènements"}
+          />
+          <Container maxWidth="lg" sx={{ mt: 2, mb: 8, ml: 6 }}>
+            <Grid container>
+              {eventInfoPage.recommendedEvents.map((event) => (
+                <Grid item md={4}>
+                  <Cards key={event.id} {...event} />
+                </Grid>
+              ))}
+            </Grid>
+          </Container>
+          </div>
         </div>
 
         ); 
